2762aaf9b94c280a73c99a3bb63153acc7a45149
Guilherme Daniel <1181743@isep.ipp.pt>
2022-02-17 17:41:14 +0000

    Autenticacao de utilizadores

--- app/config/db.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "guibfd10",
-  DB: "dei_lounge",
-  PORT: "3306"
-};
---
+++ users/user.service.js
@@ -0,0 +1,114 @@
+﻿const config = require("config.json");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const db = require("_helpers/db");
+const { param } = require("./users.controller");
+
+module.exports = {
+  authenticate,
+  getAll,
+  getById,
+  create,
+  update,
+  delete: _delete,
+};
+
+async function authenticate({ username, password }) {
+  let user = await db.User.scope("withHash").findOne({ where: { username: username } });
+
+  console.log("CHEGOU AQUI");
+  if(!user){
+     
+    user = await db.User.scope("withHash").findOne({ where: { email: username } });
+  }
+
+  //console.log("REACH");
+  if (!user || !(await bcrypt.compare(password, user.hash)))
+    throw "Username or password is incorrect";
+
+  // authentication successful
+  const token = jwt.sign({ sub: user.id }, config.secret, { expiresIn: "7d" });
+  return { ...omitHash(user.get()), token };
+}
+
+async function getAll() {
+  return await db.User.findAll();
+}
+
+async function getById(id) {
+  return await getUser(id);
+}
+
+async function create(params) {
+  // validate
+  if (await db.User.findOne({ where: { username: params.username } })) {
+    throw 'Username "' + params.username + '" is already taken';
+  }
+
+  if (await db.User.findOne({ where: { email: params.email } })) {
+    throw 'Email "' + params.email + '" is already taken';
+  }
+
+  if (params.password.length < 7) {
+    throw (
+      'Password "' + params.email + '" demasiado pequena (mínimo 8 digitos)'
+    );
+  }
+  // hash password
+  if (params.password) {
+    params.hash = await bcrypt.hash(params.password, 10);
+  }
+
+  // save user
+  await db.User.create(params);
+}
+
+async function update(id, params) {
+  const user = await getUser(id);
+
+  // validate
+  const usernameChanged = params.username && user.username !== params.username;
+  if (
+    usernameChanged &&
+    (await db.User.findOne({ where: { username: params.username } }))
+  ) {
+    throw 'Username "' + params.username + '" is already taken';
+  }
+
+  const emailChanged = params.email && user.email !== params.email;
+  if (
+    emailChanged &&
+    (await db.User.findOne({ where: { email: params.email } }))
+  ) {
+    throw 'Email "' + params.username + '" is already taken';
+  }
+
+  // hash password if it was entered
+  if (params.password) {
+    params.hash = await bcrypt.hash(params.password, 10);
+  }
+
+  // copy params to user and save
+  Object.assign(user, params);
+  await user.save();
+
+  return omitHash(user.get());
+}
+
+async function _delete(id) {
+  const user = await getUser(id);
+  await user.destroy();
+}
+
+// helper functions
+
+async function getUser(id) {
+  const user = await db.User.findByPk(id);
+  if (!user) throw "User not found";
+  return user;
+}
+
+function omitHash(user) {
+  const { hash, ...userWithoutHash } = user;
+  return userWithoutHash;
+}
